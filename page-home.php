835377f0759695feb18549e23392e58839e12b43
ViniciusOLucio <138497678+ViniciusOLucio@users.noreply.github.com>
2024-01-17 10:31:00 -0300

    desktop completo

--- page-home.php
+++ page-home.php
@@ -3,6 +3,367 @@
 ?>
 
 <?php get_header(); ?>
+<section class="hero">
+  <div class="hero-img-cor" style="--background: url('<?php echo get_template_directory_uri() ?>/assets/img/advbg1.png')">
+
+    <div class="hero-txt container sessao">
+      <div class="hero-txt-h3">
+        <div class="hero-txt-linha"></div>
+        <h3>
+          Gabriel Cristovam
+        </h3>
+      </div>
+      <h1>
+        Assistência Jurídica Especializada
+      </h1>
+      <h2>
+        15 anos de experiência em soluções jurídicas
+      </h2>
+
+    </div>
+  </div>
+</section>
+<!-- sobre -->
+<div class="col-100">
+  <div class="section-sobre container">
+    <div class="subtitulo">
+      <h4>
+        SOBRE-MIM
+      </h4>
+    </div>
+
+    <section class="sobre grid-2">
+      <div class="sobre-txt">
+        <h2>
+          Compromisso e Excelência: Gabriel Cristovam Assistência Jurídica Especializada
+        </h2>
+        <div class="sobre-txt-paragrafos">
+          <p>
+            No coração do universo jurídico, o escritório Gabriel Cristovam destaca-se como um farol de confiança e competência. Com uma equipe comprometida, trazemos décadas de experiência dedicadas à busca incansável pela justiça e defesa dos direitos de nossos clientes.
+          </p>
+          <p>
+            Nosso compromisso vai além da representação legal; é a construção de parcerias sólidas e soluções jurídicas eficazes.
+          </p>
+        </div>
+        <div class="sobre-txt_wrepper">
+          <!-- bloco1 -->
+          <div class="sobre-txt-bloco">
+            <h5>
+              +15
+            </h5>
+            <h6>
+              Prêmios Recebidos
+            </h6>
+            <p>
+              Reconhecidos pela excelência, recebemos mais de 15 prêmios que destacam nosso desempenho notável, inovação jurídica e dedicação.
+            </p>
+          </div>
+          <!-- bloco2 -->
+          <div class="sobre-txt-bloco">
+            <h5>
+              +500
+            </h5>
+            <h6>
+              Casos Bem-Sucedidos
+            </h6>
+            <p>
+              Celebramos mais de 500 casos resolvidos com sucesso, evidenciando nosso comprometimento e habilidade em alcançar resultados positivos para nossos clientes.
+            </p>
+          </div>
+        </div>
+        <div class="sobre-btn">
+          <a href="" class="btn-lermais">
+            Ler mais
+          </a>
+        </div>
+      </div>
+      <!--  -->
+      <div class="sobre-img">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/video-sobre.png" alt="">
+      </div>
+      <!--  -->
+    </section>
+
+  </div>
+  <section class="parceiros sessao">
+    <div class="parceiros-txt">
+      <h4>
+        NOSSOS PARCEIROS
+      </h4>
+      <div class="linha"></div>
+    </div>
+
+    <div class="parceiros-logos">
+      <ul class="parceiros-lista">
+        <li>
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.svg" alt="">
+        </li>
+        <li>
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.svg" alt="">
+        </li>
+        <li>
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.svg" alt="">
+        </li>
+        <li>
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.svg" alt="">
+        </li>
+        <li>
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.svg" alt="">
+        </li>
+        <li>
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.svg" alt="">
+        </li>
+      </ul>
+    </div>
+
+
+    <div class="linha"></div>
+  </section>
+  <!-- BLOG -->
+  <section class="blog container sessao grid-2">
+
+    <div class="post-destaque">
+      <h2 class="titulo-post-destaque">
+        Postagem em destaque
+      </h2>
+      <div class="post-destaque-postagem">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/post-destaque.png" alt="">
+        <h4>
+          By <span>John Doe</span> l May 23, 2022
+        </h4>
+        <h3>
+          500 Casos Vitoriosos: Gabriel Cristovam Celebra Sucesso na Defesa de Clientes
+        </h3>
+        <p>
+          Resumo: Saiba mais sobre o notável marco atingido pelo escritório, com mais de 500 casos resolvidos com sucesso, reafirmando seu compromisso com a defesa eficaz dos direitos dos clientes.
+        </p>
+        <a href="" class="btn-lermais">
+          Ler mais
+        </a>
+      </div>
+    </div>
+
+    <div class="todas-publicacoes">
+
+      <div class="titulo-todas-publicacoes">
+        <h2 class="titulo-post-destaque">
+          Todas as publicações
+        </h2>
+        <a href="">
+          Ver mais
+        </a>
+      </div>
+      <!--  -->
+      <ul>
+        <li class="publicacoes-postagem">
+
+          <h4>
+            By <span> John Doe </span>l May 23, 2022
+          </h4>
+          <h3>
+            Além das Fronteiras: Gabriel Cristovam Recebe Reconhecimento Internacional
+          </h3>
+        </li>
+        <!-- 2 -->
+        <li class="publicacoes-postagem post-ativo">
+
+          <h4>
+            By <span> John Doe </span>l May 23, 2022
+          </h4>
+          <h3>
+          500 Casos Vitoriosos: Gabriel Cristovam Celebra Sucesso na Defesa de Clientes
+          </h3>
+        </li>
+        <!-- 3 -->
+        <li class="publicacoes-postagem">
+
+          <h4>
+            By <span> John Doe </span>l May 23, 2022
+          </h4>
+          <h3>
+          Premiações e Conquistas: Gabriel Cristovam Destaca-se com +15 Prêmios
+          </h3>
+        </li>
+        <!-- 4 -->
+        <li class="publicacoes-postagem">
+
+          <h4>
+            By <span> John Doe </span>l May 23, 2022
+          </h4>
+          <h3>
+          Gabriel Cristovam: Mais de Duas Décadas de Jurisprudência e Excelência
+          </h3>
+        </li>
+      </ul>
+    </div>
+
+
+
+  </section>
+  <!-- BLOG -->
+  <section class="comentarios container sessao">
+    <div class="subtitulo">
+      <h4>
+        COMENTARIOS
+      </h4>
+    </div>
+
+    <!--  -->
+    <div class="comentarios-titulo">
+      <h2>
+        O Que as Pessoas Estão Dizendo <br> Sobre mim
+      </h2>
+    </div>
+    <div class="comentarios-caixas-ul">
+      <ul class="comentarios-caixas">
+        <li class="comentario-caixa">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/aspas.svg" alt="">
+          <p>
+            Impressionada com a trajetória de sucesso do escritório Gabriel Cristovam.
+          </p>
+          <div class="comentario-perfil">
+            <img src="<?php echo get_template_directory_uri() ?>/assets/img/perfil.png" alt="">
+            <div class="comentario-perfil-txt">
+              <h3>
+                Luciana Santos
+              </h3>
+              <h4>
+                Advogada Direito Empresarial
+              </h4>
+            </div>
+          </div>
+        </li>
+        <!-- card2 -->
+        <li class="comentario-caixa">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/aspas.svg" alt="">
+          <p>
+            Impressionada com a trajetória de sucesso do escritório Gabriel Cristovam.
+          </p>
+          <div class="comentario-perfil">
+            <img src="<?php echo get_template_directory_uri() ?>/assets/img/perfil.png" alt="">
+            <div class="comentario-perfil-txt">
+              <h3>
+                Luciana Santos
+              </h3>
+              <h4>
+                Advogada Direito Empresarial
+              </h4>
+            </div>
+          </div>
+        </li>
+        <!-- caRd 3 -->
+        <li class="comentario-caixa">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/aspas.svg" alt="">
+          <p>
+            Impressionada com a trajetória de sucesso do escritório Gabriel Cristovam.
+          </p>
+          <div class="comentario-perfil">
+            <img src="<?php echo get_template_directory_uri() ?>/assets/img/perfil.png" alt="">
+            <div class="comentario-perfil-txt">
+              <h3>
+                Luciana Santos
+              </h3>
+              <h4>
+                Advogada Direito Empresarial
+              </h4>
+            </div>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+
+  </section>
+</div>
+
+
+<div class="contato-bg">
+
+  <section class="contato container grid-2 sessao">
+    <!-- bloco1 -->
+    <div class="contato-txt">
+      <h2>
+        Levamos a assistência jurídica a sério.
+      </h2>
+
+      <div>
+        <ul class="contato-txt-lista">
+          <li class="contato-topico-txt">
+            <div class="contato-topico-img">
+              <img src="<?php echo get_template_directory_uri() ?>/assets/img/Icon-contact.svg" alt="">
+            </div>
+            <div class="contato-bloco-txt">
+              <h3>
+                Atendimento Personalizado
+              </h3>
+              <p>
+                Through True Rich Attended does no end it his mother since real had half every him.
+              </p>
+            </div>
+          </li>
+          <!-- topico2 -->
+          <li class="contato-topico-txt">
+            <div class="contato-topico-img">
+              <img src="<?php echo get_template_directory_uri() ?>/assets/img/Icon-topico3.svg  " alt="">
+            </div>
+            <div class="contato-bloco-txt">
+              <h3>
+              Transparência nos Honorários
+              </h3>
+              <p>
+              Detalhes sobre nossa política de preços transparentes, visando fornecer clareza e honestidade
+              </p>
+            </div>
+          </li>
+          <!-- topico3 -->
+          <li class="contato-topico-txt">
+            <div class="contato-topico-img">
+              <img src="<?php echo get_template_directory_uri() ?>/assets/img/Icon-topico2.svg" alt="">
+            </div>
+            <div class="contato-bloco-txt">
+              <h3>
+              Especialização Jurídica Abrangente
+              </h3>
+              <p>
+              Informações sobre a ampla gama de áreas jurídicas em que atuamos, destacando nossa especialização.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+    <!-- bloco2 -->
+    <div class="contato-formulario">
+      <h2>
+        Obtenha um Orçamento para a Assistência Jurídica
+      </h2>
+
+      <div class="contact-form">
+
+        <form action="/enviar" method="post">
+          <!-- <label for="nome">Nome:</label> -->
+          <input type="text" id="nome" name="nome" required placeholder="Nome:">
+
+          <!-- <label for="email">Email:</label> -->
+          <input type="email" id="email" name="email" required placeholder="Email:">
+
+          <!-- <label for="assunto">Assunto:</label> -->
+          <input type="text" id="assunto" name="assunto" required placeholder="Assunto:">
+
+          <!-- <label for="mensagem">Mensagem:</label> -->
+          <textarea id="mensagem" name="mensagem" rows="4" required placeholder="Mensagem:"></textarea>
+
+          <button type="submit" class="btn-form">Enviar</button>
+        </form>
+      </div>
+
+
+
+    </div>
+  </section>
+
+</div>
 
 
 
